f9c37b72d9b9ddc2842bff9fd51e1136133daa42
Gebes <35232234+GebesPlays@users.noreply.github.com>
2020-06-27 19:23:11 +0200

    Added time

--- src/eu/gebes/Main.java
+++ src/eu/gebes/Main.java
@@ -5,25 +5,30 @@ import java.math.BigInteger;
 public class Main {
 
     public static void main(String[] args) {
-        infinityRecord();
+       infinityRecord();
     }
 
-    private static void checkSpecificNumber(BigInteger i){
-        System.out.println(i +": " + steps(i));
+    private static void checkSpecificNumber(BigInteger i) {
+        System.out.println(i + ": " + steps(i));
     }
 
-    private static void infinityRecord(){
+    private static void infinityRecord() {
         int longest = 0;
 
         long start = System.currentTimeMillis();
+        long last = System.currentTimeMillis();
 
         for (int i = 1; true; i++) {
 
             var steps = steps(BigInteger.valueOf(i));
 
-            if(steps > longest) {
+            if (steps > longest) {
                 longest = steps;
-                System.out.println("New record " + i + ": " + steps + " in " +((System.currentTimeMillis() - start) / 1000f) + "seconds");
+
+                float lastTime = ((System.currentTimeMillis() - last) / 1000f);
+                float startTime = ((System.currentTimeMillis() - start) / 1000f);
+                System.out.println("Found new record " + String.format("%10d", i) + ": " + String.format("%5d", steps) + "\t\tafter " + String.format("%2.3f", lastTime) + "s \t\ttime since start " + String.format("%2.3f", startTime) + "s");
+                last = System.currentTimeMillis();
             }
 
             //  System.out.println(i);
@@ -33,16 +38,16 @@ public class Main {
 
     }
 
-    private static void record(){
+    private static void record() {
 
         int longest = 0;
 
         for (int i = 1; i < 1000000; i++) {
 
-            if(steps(BigInteger.valueOf(i)) > longest)
+            if (steps(BigInteger.valueOf(i)) > longest)
                 longest = i;
 
-          //  System.out.println(i);
+            //  System.out.println(i);
 
         }
 
@@ -50,18 +55,17 @@ public class Main {
 
     }
 
-    private static int steps(BigInteger count){
+    private static int steps(BigInteger count) {
 
         int steps = 1;
 
-        while (!count.equals(BigInteger.ONE)){
-            if(count.mod(BigInteger.TWO).equals(BigInteger.ZERO)){
+        while (!count.equals(BigInteger.ONE)) {
+            if (count.mod(BigInteger.TWO).equals(BigInteger.ZERO)) {
                 count = count.divide(BigInteger.TWO);
-            }else{
+            } else {
                 count = count.multiply(BigInteger.valueOf(3)).add(BigInteger.ONE);
             }
-            steps ++;
-
+            steps++;
         }
 
         return steps;
